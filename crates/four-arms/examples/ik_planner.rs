09780e4ea6f1203d50805bd0d1f52f558fc9750d
Sachin Kumar <sachinkum123567@gmail.com>
2026-05-31 19:18:42 +0200

    enhance: replaced hardcoded path with runtime input
    Signed-off-by: Sachin Kumar <sachinkum123567@gmail.com>

--- crates/four-arms/examples/ik_planner.rs
+++ crates/four-arms/examples/ik_planner.rs
@@ -1,10 +1,13 @@
 use four_arms::chain::Chain;
 use four_arms::errors::FourArmError;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), FourArmError> {
-    let file_path = "/Users/mac/zzzzz/rust/robotics/robotics/urdf/my_robot2.urdf";
-    let chain = Chain::from_urdf(file_path)?;
+    let file_path = env::args()
+        .nth(0)
+        .unwrap_or_else(|| "urdf/my_robot2.urdf".to_string());
+    let chain = Chain::from_urdf(&file_path)?;
 
     let joints1 = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
     let pose = chain.forward_kinematics(&joints1)?;
